9a7603024f93cc92e5159cc2280232c9514fee5b
Jordan <jordan.rouco-miguez@epitech.eu>
2021-07-16 14:15:03 +0200

    Update backupPusher.sh

--- backupPusher.sh
+++ backupPusher.sh
@@ -2,7 +2,7 @@ if [ -f .env ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs)
   mysqldump -u$USERNAME -p$PASSWORD --all-databases > ../$PATH/$WEBSITE/public_html/$WEBSITE.sql
-  cd ../$PATH/$WEBSITE/public_html
+  cd $PATH/$WEBSITE/public_html
   git commit -am "Automatic backup commit"
   git push
 fi
