5bfd01d16416b13b5eee0bcc7a9ca37209b1bfed
Customer <u65-94rq6qgllhzh@c87996.sgvps.net>
2021-08-13 10:23:33 +0000

    Automatic backup commit

--- backupPusher.sh
+++ backupPusher.sh
@@ -1,8 +1,8 @@
 if [ -f .env ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs)
-  mysqldump -u$USERNAME -p$PASSWORD --all-databases > $PATH/$WEBSITE/public_html/$WEBSITE.sql
-  cd $PATH/$WEBSITE/public_html
-  git commit -am "Automatic backup commit"
-  git push
+  /bin/mysqldump -u$USERNAME -p$PASSWORD --all-databases > $PATH/$WEBSITE.sql
+  cd $PATH
+  /bin/git commit -am "Automatic backup commit"
+  /bin/git push
 fi
