16628b8173c64c081628004f6fa8e2d78e460acd
Jordan <jordan.rouco-miguez@epitech.eu>
2021-10-20 12:15:08 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,10 +1,14 @@
 REMOTE=$1
 
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+cat ~/.ssh/id_rsa.pub
+echo "Copier cette clé publique avant de continuer dans https://fructiweb.synology.me:3000/user/settings/ssh"
+read -n 1 -s -r -p "Presser n'importe quel bouton si la clé est correctement copié"
 cd ../public_html
 git init
 git config --global core.preloadIndex false
 git remote add origin $1
+echo "Adding files to commit"
 git add *
 git commit -m "initial commit"
 git push -u origin master
