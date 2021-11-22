2678e789d55b3ab4a5db6dd7368ff8de89d56a7f
Jordan <jordan.rouco-miguez@epitech.eu>
2021-11-22 16:34:52 +0100

    adding new executable component

---
+++ storageChecker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STORAGE=`du -sh | tr -dc '0-9'`
+GIT=`du -sh public_html/.git | tr -dc '0-9'`
+RESULT=$(($STORAGE-$GIT))
+
+read -r -d '' MERGE << EOM
+{
+        "dns": "$1",
+        "storage": "$RESULT"
+}
+EOM
+
+curl -X POST --data "$MERGE" -H "Content-Type: application/json" -H "X-Storage-Delivery: yes" -H "X-Storage-Event: push" https://app.fructiweb.com/api/storageWebhook
