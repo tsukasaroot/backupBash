4102c379669b095d9dafc6b98601f843f2504e3e
Jordan <jordan.rouco-miguez@epitech.eu>
2021-11-23 15:45:24 +0100

    Update backupPusher.sh

--- backupPusher.sh
+++ backupPusher.sh
@@ -7,4 +7,4 @@ mysqldump -u$USERNAME -p$PASSWORD --all-databases > ../public_html/$WEBSITE.sql
 cd ../public_html
 git add *
 git commit -m "Automatic backup commit"
-git push -u origin master
+git push -f -u origin master
